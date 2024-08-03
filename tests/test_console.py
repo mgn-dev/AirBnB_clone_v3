6017892ee654625921cac4076a269022c4d20e03
emjay026 <126557401+mgn-dev@users.noreply.github.com>
2024-08-03 11:44:05 +0200

    Do initial commit, project 0x05. AirBnB clone - RESTful API

--- tests/test_console.py
+++ tests/test_console.py
@@ -3,39 +3,44 @@
 Contains the class TestConsoleDocs
 """
 
-import console
 import inspect
-import pep8
 import unittest
-HBNBCommand = console.HBNBCommand
+import pycodestyle
+from console import HBNBCommand
+import console
 
 
 class TestConsoleDocs(unittest.TestCase):
     """Class for testing documentation of the console"""
-    def test_pep8_conformance_console(self):
+
+    def test_pep8_conformance_console(self) -> None:
         """Test that console.py conforms to PEP8."""
-        pep8s = pep8.StyleGuide(quiet=True)
-        result = pep8s.check_files(['console.py'])
+        style = pycodestyle.StyleGuide(quiet=True)
+        result = style.check_files(['console.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
 
-    def test_pep8_conformance_test_console(self):
+    def test_pep8_conformance_test_console(self) -> None:
         """Test that tests/test_console.py conforms to PEP8."""
-        pep8s = pep8.StyleGuide(quiet=True)
-        result = pep8s.check_files(['tests/test_console.py'])
+        style = pycodestyle.StyleGuide(quiet=True)
+        result = style.check_files(['tests/test_console.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
 
-    def test_console_module_docstring(self):
+    def test_console_module_docstring(self) -> None:
         """Test for the console.py module docstring"""
-        self.assertIsNot(console.__doc__, None,
-                         "console.py needs a docstring")
+        self.assertIsNotNone(console.__doc__,
+                             "console.py needs a docstring")
         self.assertTrue(len(console.__doc__) >= 1,
                         "console.py needs a docstring")
 
-    def test_HBNBCommand_class_docstring(self):
+    def test_HBNBCommand_class_docstring(self) -> None:
         """Test for the HBNBCommand class docstring"""
-        self.assertIsNot(HBNBCommand.__doc__, None,
-                         "HBNBCommand class needs a docstring")
+        self.assertIsNotNone(HBNBCommand.__doc__,
+                             "HBNBCommand class needs a docstring")
         self.assertTrue(len(HBNBCommand.__doc__) >= 1,
                         "HBNBCommand class needs a docstring")
+
+
+if __name__ == "__main__":
+    unittest.main()
