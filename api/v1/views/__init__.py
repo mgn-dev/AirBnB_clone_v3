7d5b756da5a551dc1836409ec1855cb469cbcb4f
emjay026 <126557401+mgn-dev@users.noreply.github.com>
2024-08-05 21:39:09 +0200

    Do task 3, project 0x05. AirBnB clone - RESTful API

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,8 +2,9 @@
 """Package initializer"""
 
 from flask import Blueprint
-# Wildcard import everything in the package api.v1.views.index
-from api.v1.views.index import *
 
 # Create an instance of Blueprint
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+# Wildcard import everything in the package api.v1.views.index
+from api.v1.views.index import *
